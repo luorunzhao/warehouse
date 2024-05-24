7c9e80b89c8bdbcab652bf0248b0c1ac90a191fa
luorunzhao <2592155140@qq.com>
2024-05-24 16:02:08 +0800

    init project

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
-import './assets/main.css'
-
+import 'element-plus/dist/index.css'
+import ElementPlus from 'element-plus'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
@@ -7,5 +7,5 @@ import router from './router'
 const app = createApp(App)
 
 app.use(router)
-
+app.use(ElementPlus)
 app.mount('#app')
